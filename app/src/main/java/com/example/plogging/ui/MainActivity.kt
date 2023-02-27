5f7eac766bf9e1e668e474625d5081f8b649fa89
INAH-PAK <inahpakkr@gmail.com>
2023-02-27 21:19:49 +0900

    feat: 로그인 - 로그인 화면 레이아웃 구현, MainActivity 추가

---
+++ app/src/main/java/com/example/plogging/ui/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.plogging.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.plogging.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
