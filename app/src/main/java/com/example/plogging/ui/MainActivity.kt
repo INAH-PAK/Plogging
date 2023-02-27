718abe758dc0b3f260056974961cde76db0db6cc
INAH-PAK <inahpakkr@gmail.com>
2023-02-27 23:39:13 +0900

    feat: navigation graph와 Bottom Navigation 메뉴 연결

--- app/src/main/java/com/example/plogging/ui/MainActivity.kt
+++ app/src/main/java/com/example/plogging/ui/MainActivity.kt
@@ -1,12 +1,27 @@
 package com.example.plogging.ui
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.fragment.findNavController
+import androidx.navigation.ui.setupWithNavController
 import com.example.plogging.R
+import com.example.plogging.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        setLayout()
+    }
+
+    private fun setLayout() {
+        val navController =
+            supportFragmentManager.findFragmentById(R.id.nav_host_fragment)?.findNavController()
+        navController?.let {
+            binding.homeBottomNavigation.setupWithNavController(it)
+        }
     }
 }
