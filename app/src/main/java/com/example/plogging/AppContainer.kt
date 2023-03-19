31492e2e2e608071e837710e83dec73d970f55a6
INAH-PAK <inahpakkr@gmail.com>
2023-03-19 13:43:19 +0900

    feat: 앱 전역에서 사용하도록 데이터 베이스 instance를 AppContainer에 싱글톤으로 추가

--- app/src/main/java/com/example/plogging/AppContainer.kt
+++ app/src/main/java/com/example/plogging/AppContainer.kt
@@ -2,6 +2,7 @@ package com.example.plogging
 
 import android.app.Application
 import com.example.plogging.data.source.UserDataRepository
+import com.example.plogging.data.source.local.AppDatabase
 import com.example.plogging.data.source.remote.ApiClient
 import com.example.plogging.data.source.remote.PreferenceManager
 
@@ -10,6 +11,11 @@ class AppContainer(private val application: Application) {
     private var apiClient: ApiClient? = null
     private var dataStore: PreferenceManager? = null
     private var userDataRepository: UserDataRepository? = null
+    private var database: AppDatabase? = null
+
+    fun provideAppDatabase(): AppDatabase {
+        return AppDatabase.getInstance(application)
+    }
 
     fun provideApiClient(): ApiClient {
         return apiClient ?: ApiClient.create(BuildConfig.KAKAO_REST_API_KEY).apply {
