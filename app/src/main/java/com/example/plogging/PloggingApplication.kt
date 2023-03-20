fb40a13cfc505caaa95e01485912d616c8c438c2
INAH-PAK <inahpakkr@gmail.com>
2023-03-20 16:05:32 +0900

    feat: SDK 26 이상일 경우, Application 하위 클래스에서 Notification Channel 생성

--- app/src/main/java/com/example/plogging/PloggingApplication.kt
+++ app/src/main/java/com/example/plogging/PloggingApplication.kt
@@ -1,6 +1,10 @@
 package com.example.plogging
 
 import android.app.Application
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.os.Build
+import android.util.Log
 import com.naver.maps.map.NaverMapSdk
 
 class PloggingApplication : Application() {
@@ -10,9 +14,25 @@ class PloggingApplication : Application() {
         NaverMapSdk.getInstance(this).client =
             NaverMapSdk.NaverCloudPlatformClient(BuildConfig.NAVER_CLIENT_ID)
         appContainer = AppContainer(this)
+
+        createNotificationChannel()
+    }
+
+    private fun createNotificationChannel() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val name = getString(R.string.channel_name)
+            val descriptionText = getString(R.string.channel_description)
+            val importance = NotificationManager.IMPORTANCE_HIGH
+            val mChannel = NotificationChannel(CHANNEL_ID, name, importance)
+            mChannel.description = descriptionText
+            val notificationManager = getSystemService(NOTIFICATION_SERVICE) as NotificationManager
+            notificationManager.createNotificationChannel(mChannel)
+            Log.i(" 채널이 만들어 졌는가? ", notificationManager.areNotificationsEnabled().toString())
+        }
     }
 
     companion object {
         lateinit var appContainer: AppContainer
+        const val CHANNEL_ID = "CHANNEL_GPS"
     }
 }
