2ce05ea8d300c079f9c16459216906f96b3cab74
INAH-PAK <inahpakkr@gmail.com>
2023-03-21 14:01:44 +0900

    refectory : 채널 ID 변경 -> PLOGGING_CHANNEL_ID

--- app/src/main/java/com/example/plogging/PloggingApplication.kt
+++ app/src/main/java/com/example/plogging/PloggingApplication.kt
@@ -4,7 +4,7 @@ import android.app.Application
 import android.app.NotificationChannel
 import android.app.NotificationManager
 import android.os.Build
-import android.util.Log
+import com.example.plogging.util.Constants
 import com.naver.maps.map.NaverMapSdk
 
 class PloggingApplication : Application() {
@@ -16,6 +16,7 @@ class PloggingApplication : Application() {
         appContainer = AppContainer(this)
 
         createNotificationChannel()
+
     }
 
     private fun createNotificationChannel() {
@@ -23,16 +24,14 @@ class PloggingApplication : Application() {
             val name = getString(R.string.channel_name)
             val descriptionText = getString(R.string.channel_description)
             val importance = NotificationManager.IMPORTANCE_HIGH
-            val mChannel = NotificationChannel(CHANNEL_ID, name, importance)
+            val mChannel = NotificationChannel(Constants.PLOGGING_CHANNEL_ID, name, importance)
             mChannel.description = descriptionText
             val notificationManager = getSystemService(NOTIFICATION_SERVICE) as NotificationManager
             notificationManager.createNotificationChannel(mChannel)
-            Log.i(" 채널이 만들어 졌는가? ", notificationManager.areNotificationsEnabled().toString())
         }
     }
 
     companion object {
         lateinit var appContainer: AppContainer
-        const val CHANNEL_ID = "CHANNEL_GPS"
     }
 }
